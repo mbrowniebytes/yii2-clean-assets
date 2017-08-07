bc37ebdb70cf614445f82eb77cda865d5b0b0f1b
mbrowniebytes <>
2017-08-07 14:27:58 -0500

    issue-1 support advanced template
    add option structure to auto set asset dirs
    add option dirs to set custom asset dirs
    refactored code into more methods
    api breaking change cleanAssetDir now takes a dir param
    api breaking change cleanAssetDirs should be used in code instead of
    prior clean AssetDir

--- CleanAssetsController.php
+++ CleanAssetsController.php
@@ -11,17 +11,58 @@ use Yii;
  */
 class CleanAssetsController extends Controller
 {
+    // show what would happen; do not delete anything
     public $dry_run = false;
+    
+    // show more info; echo rules being run, dir/file being deleted
     public $verbose = false;
+    
+    // do not echo anything
     public $silent = false;
-    /*
-     * nbr asset dirs to keep; might be one or more for app, one for toolbar, etc
-     */
+    
+    // nbr asset dirs to keep; might be one or more for app, one for toolbar, etc
     public $keep = 5;
-    public $asset_dir = 'web/assets';
+    
+    // based on yii2 recommended structures, set asset dirs to clean
+    // advanced, basic, auto (both)
+    public $structure = 'auto';
+    
+    // list of custom asset dirs to clean, comma seperated
+    public $dirs = [];
 
 
+    /**
+     * console controller
+     * 
+     * @return int
+     */
     public function actionIndex()
+    {
+        try {            
+            $this->parseRequestedParams();
+
+            $this->setAssetDirs();
+
+            $this->cleanAssetDirs();
+            
+        } catch (Exception $e) {
+            echo $e->getMessage()."\n";
+            return $e->getCode();
+        }
+        return 0;
+    }
+
+    private function echo_msg($msg, $show=true)
+    {
+        if (!$this->silent && ($this->dry_run || $this->verbose || $show)) {
+            echo $msg."\n";
+        }
+    }
+
+    /**
+     * parse the requested params
+     */
+    private function parseRequestedParams() 
     {
         foreach (Yii::$app->requestedParams as $param) {
             if (stripos($param, 'dry-run') !== false || stripos($param, 'dryrun') !== false) {
@@ -33,36 +74,68 @@ class CleanAssetsController extends Controller
             } else if (stripos($param, 'keep=') !== false) {
                 $keep = substr($param, strpos($param, '=') + 1);
                 $this->keep = (int)$keep;
+            } else if (stripos($param, 'structure=') !== false) {
+                $structure = substr($param, strpos($param, '=') + 1);
+                if (!in_array($structure, ['advanced', 'auto', 'basic'])) {
+                  $this->structure = 'auto';
+                }
+                $this->structure = $structure;
+            } else if (stripos($param, 'dirs=') !== false) {
+                $dirs = substr($param, strpos($param, '=') + 1);
+                $this->dirs = explode(',', $dirs);
             }
         }
-
-        $this->echo_msg('Checking '.$this->asset_dir.'/ to remove old caches .. ');
-
-        $nbr_cleaned = self::cleanAssetDir();
-
-        $this->echo_msg('Done. Removed '.$nbr_cleaned.' '.$this->asset_dir.'/ cache'.($nbr_cleaned == 1 ? '' : 's'));
-
-        return 0;
     }
-
-    private function echo_msg($msg, $show=true)
+    
+    /**
+     * set asset dirs
+     */
+    private function setAssetDirs() 
     {
-        if (!$this->silent && ($this->dry_run || $this->verbose || $show)) {
-            echo $msg."\n";
+        if ($this->structure == 'basic') {
+            array_push($this->dirs, 'web/assets');
+        } else if ($this->structure == 'advanced') {
+            array_push($this->dirs, 'frontend/web/assets');
+            array_push($this->dirs, 'backend/web/assets');
+        } else { // 'auto'
+            array_push($this->dirs, 'web/assets');
+            array_push($this->dirs, 'frontend/web/assets');
+            array_push($this->dirs, 'backend/web/assets');
         }
     }
-
-    /*
-     * remove prior asset dirs
-     *
-     * @return void
+    
+    /**
+     * clean all asset dirs
      */
-    public function cleanAssetDir()
+    public function cleanAssetDirs()
+    {
+        foreach ($this->dirs as $asset_dir) {
+            if (!is_dir($asset_dir)) {
+                $this->echo_msg('Did not find '.$asset_dir.'/ .. skipping');
+                continue;
+            }
+            $this->echo_msg('Checking '.$asset_dir.'/ to remove old caches .. ');
+
+            $nbr_cleaned = self::cleanAssetDir($asset_dir);
+
+            $this->echo_msg('Removed '.$nbr_cleaned.' '.$asset_dir.'/ cache'.($nbr_cleaned == 1 ? '' : 's'));
+        }
+        $this->echo_msg('Finished');
+    }
+    
+    /**
+     * clean asset dir
+     * may remove subdirs in asset dir 
+     *
+     * @param string $asset_dir
+     * @return int
+     */
+    public function cleanAssetDir($asset_dir)
     {
 
         $now = time();
 
-        $asset_temp_dirs = glob($this->asset_dir . '/*' , GLOB_ONLYDIR);
+        $asset_temp_dirs = glob($asset_dir . '/*' , GLOB_ONLYDIR);
 
         // check if less than want to keep
         if (count($asset_temp_dirs) <= $this->keep) {
